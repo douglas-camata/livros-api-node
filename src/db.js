38f20172752786788408519aaf52c69c55c22a24
Douglas Camata de Araujo <douglas.camata@gmail.com>
2024-07-24 11:01:38 -0300

    teste com postgreSQL

---
+++ src/db.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+
+// Crie uma nova pool de conexões com o banco de dados
+const pool = new Pool({
+  user: 'postgres.vxoawppcwosabhsqppsm',
+  host: 'aws-0-sa-east-1.pooler.supabase.com',
+  database: 'postgres',
+  password: 'T@p$06supabase',
+  port: 6543,
+});
+
+// Exporta a pool para uso em outras partes da aplicação
+module.exports = pool;
