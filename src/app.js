d1041dca1db91d1b38c50faac4ea376174078e12
Douglas Camata de Araujo <douglas.camata@gmail.com>
2024-07-24 11:13:54 -0300

    Criação de rota e página de view

--- src/app.js
+++ src/app.js
@@ -2,44 +2,26 @@ const express = require('express');
 const app = express();
 const pool = require('./db');
 
+const livrosRoutes = require('./routes/livros')
+
 app.use(express.json());
 
-let livros = []
+// Configura o diretório de views e o motor de templates EJS
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.json(livros)
-})
+app.use('/api', livrosRoutes)
 
-app.post('/', (req, res) => {
-    const { titulo, autor } = req.body;
-    const novoLivro = { id: livros.length + 1, titulo, autor };
-    livros.push(novoLivro);
-    res.status(201).json(novoLivro);
-});
-
-app.get('/livros', async (req, res) => {
+// Rota para exibir os livros cadastrados
+app.get('/', async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM livros');
-        res.json(result.rows);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-// Rota para adicionar um novo livro
-app.post('/livros', async (req, res) => {
-    try {
-        const { titulo, autor } = req.body;
-        const result = await pool.query(
-            'INSERT INTO livros (titulo, autor) VALUES ($1, $2) RETURNING *',
-            [titulo, autor]
-        );
-        res.status(201).json(result.rows[0]);
+        const { rows } = await pool.query('SELECT * FROM livros');
+        res.render('index', { livros: rows });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 });
 
 app.listen(3000, () => {
-    console.log('Servidor rodando na porta http://localhost:3000');
+    console.log('Servidor rodando na porta 3000');
 });
