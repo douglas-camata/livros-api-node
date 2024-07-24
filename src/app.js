efc8d75ec1b8e86ddbbe76fa94c904f867180d82
Douglas Camata de Araujo <douglas.camata@gmail.com>
2024-07-24 11:18:08 -0300

    teste

--- src/app.js
+++ src/app.js
@@ -6,21 +6,21 @@ const livrosRoutes = require('./routes/livros')
 
 app.use(express.json());
 
-// Configura o diretório de views e o motor de templates EJS
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// // Configura o diretório de views e o motor de templates EJS
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
 app.use('/api', livrosRoutes)
 
 // Rota para exibir os livros cadastrados
-app.get('/', async (req, res) => {
-    try {
-        const { rows } = await pool.query('SELECT * FROM livros');
-        res.render('index', { livros: rows });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
+// app.get('/', async (req, res) => {
+//     try {
+//         const { rows } = await pool.query('SELECT * FROM livros');
+//         res.render('index', { livros: rows });
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
