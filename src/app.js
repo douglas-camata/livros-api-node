486dd909e5c4007f45d2a5d12d5877578ec6b61e
Douglas Camata de Araujo <douglas.camata@gmail.com>
2024-07-24 11:19:16 -0300

    teste

--- src/app.js
+++ src/app.js
@@ -10,7 +10,41 @@ app.use(express.json());
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'ejs');
 
-app.use('/api', livrosRoutes)
+let livros = []
+
+app.get('/', (req, res) => {
+    res.json(livros)
+})
+
+app.post('/', (req, res) => {
+    const { titulo, autor } = req.body;
+    const novoLivro = { id: livros.length + 1, titulo, autor };
+    livros.push(novoLivro);
+    res.status(201).json(novoLivro);
+});
+
+app.get('/livros', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM livros');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Rota para adicionar um novo livro
+app.post('/livros', async (req, res) => {
+    try {
+        const { titulo, autor } = req.body;
+        const result = await pool.query(
+            'INSERT INTO livros (titulo, autor) VALUES ($1, $2) RETURNING *',
+            [titulo, autor]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
 // Rota para exibir os livros cadastrados
 // app.get('/', async (req, res) => {
